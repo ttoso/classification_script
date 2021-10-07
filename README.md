847d0c9295ef1f63eab4359cce64937046c60602
ttoso <aroman@digital.ai>
2021-10-08 00:37:33 +0200

    First version

---
+++ README.md
@@ -0,0 +1,16 @@
+# Classification script for excel
+This script allows to classify a xlxs file with two columns and store the rows in a json file with 16 categories
+
+## Install
+```
+pip install -r requirements.txt
+```
+## Use
+The data.json file must have the provided structure to classify in the categories.
+```
+python classification_script.py
+```
+or
+```
+python3 classification_script.py
+```
---
+++ classification_script.py
@@ -0,0 +1,94 @@
+
+
+import pylightxl as xl
+import json
+
+# 01. AGRICULTURA, PESCA Y GANADERIA
+# 02. CIENCIA Y TECNOLOGÍA
+# 03. DEFENSA
+# 04. DERECHO Y JUSTICIA
+# 05. ECONOMÍA Y COMERCIO
+# 06. EDUCACIÓN Y CULTURA
+# 07. HACIENDA
+# 08. IGLESIA Y RELIGIÓN
+# 09. INDUSTRIA
+# 10. POLÍTICA Y ADMINISTRACIÓN PÚBLICA
+# 11. SOCIEDAD Y TRABAJO
+# 12. MEDIO AMBIENTE
+# 13. DOCUMENTACIÓN E INFORMACIÓN
+# 14. BIENES CULTURALES
+# 15. EVENTOS
+# 16. ARQUITECTURA Y URBANISMO]
+
+def classify(code:str, name:str) -> bool:
+    data_saved = True
+    category = int(input(f'{name} '))
+
+    data = {code: name}
+
+    if category == 1:
+        update_json(str(category), data)
+    elif category ==2:
+        update_json(str(category), data)
+    elif category ==3:
+        update_json(str(category), data)
+    elif category ==4:
+        update_json(str(category), data)
+    elif category ==5:
+        update_json(str(category), data)
+    elif category ==6:
+        update_json(str(category), data)
+    elif category ==7:
+        update_json(str(category), data)
+    elif category ==8:
+        update_json(str(category), data)
+    elif category ==9:
+        update_json(str(category), data)
+    elif category ==10:
+        update_json(str(category), data)
+    elif category ==11:
+        update_json(str(category), data)
+    elif category ==12:
+        update_json(str(category), data)
+    elif category ==13:
+        update_json(str(category), data)
+    elif category ==14:
+        update_json(str(category), data)
+    elif category ==15:
+        update_json(str(category), data)
+    elif category ==16:
+        update_json(str(category), data)
+    else:
+        print('Buen intento crack')
+        data_saved = False
+
+    if data_saved:
+        print('Fila guardada correctamente')
+    return data_saved
+
+
+def update_json(category: str, data: dict) -> None:
+    with open('data.json') as json_file:
+        json_data = json.load(json_file)
+
+    json_data[category].append(data)
+
+    with open('data.json', 'w', encoding='utf-8') as json_file:
+        json.dump(json_data, json_file, indent=4, ensure_ascii=False)
+
+def main() -> None:
+    # readxl returns a pylightxl database that holds all worksheets and its data
+    db = xl.readxl(fn='PA.xlsx')
+
+    # return all sheetnames
+    codes = db.ws(ws='Hoja1').col(col=1)
+    names = db.ws(ws='Hoja1').col(col=2)
+
+    for code, name in zip(codes, names):
+        write_success = False
+        while not write_success:
+            write_success = classify(code, name)
+
+
+if __name__ == "__main__":
+    main()
